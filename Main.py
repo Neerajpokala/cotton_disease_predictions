c9188e49cf60373e123cec4fe38025ca31b1046b
neerajpokala143 <73011282+neerajpokala143@users.noreply.github.com>
2024-02-23 16:45:33 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -75,7 +75,7 @@ def main():
             prediction = predict_image_class(model, img_array)
             st.success(f"Prediction: {prediction}")
             if prediction in ['diseased cotton leaf', 'diseased cotton plant']:
-                st.markdown(f"**The predicted Disease name is {random.choice(cotton_diseases)}"**)
+                st.markdown(f"The predicted Disease name is {random.choice(cotton_diseases)}")
         
 
 if __name__ == "__main__":
